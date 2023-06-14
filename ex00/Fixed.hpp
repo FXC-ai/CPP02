311a076ca463218300d49cf3fe0bebc6b5e9c743
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-06-14 15:23:29 +0200

    Plus rien fonctionne

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -7,27 +7,17 @@ class Fixed
 {
 	public:
 		Fixed();
-		Fixed(int const n);
-		Fixed(float const n);
+		Fixed(int n);
 		Fixed(Fixed const &src);
 		~Fixed();
-
 		Fixed&	operator=(Fixed const &rhs);
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
-		float	toFloat(void) const;		
-		int		toInt(void) const;
 
 	private:
-		int			_RawBits;
+		int					_RawBits;
 		int	const	_IndComma;
 };
 
-std::ostream &	operator<<(std::ostream& o, Fixed const & i);
-
-/*
-
-
-*/
 
 #endif
--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -1,20 +1,53 @@
 #include "Fixed.hpp"
 
-Fixed&	Fixed::operator=(Fixed const &rhs)
-{
-	std::cout << "Copy assignment operator called" << std::endl;
-	this->_RawBits = rhs.getRawBits();
-	return *this;
-}
-
 Fixed::Fixed() : _RawBits(0), _IndComma(8)
 {
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(int n) : _RawBits(n), _IndComma(8){}
+Fixed::Fixed(int const n): _IndComma(8)
+{
+	this->_RawBits = (n << this->_IndComma);
+	std::cout << "Int constructor called" << std::endl;
+}
 
-Fixed::Fixed(Fixed const &src) : _IndComma(8)
+Fixed::Fixed(float const n): _IndComma(8)
+{
+	/*
+		Dans le main on instacie un objet ainsi : Fixed c(42.42)
+		Pour obtenir la valeur de RawBit :
+			1) On calcule le_float_en_parametre * 256 = 42.42 * 256 = 10859.52
+			2) On caste 10859.52 dans RawBit, il va stocker 10859
+			MAIS ! Pour plus de precision il serait préférable d'arrondir avant de 
+			caster dans in int.
+			Du coup...
+			2) roundf(10859.52) renvoie 10860
+			3) On caste 10860 dans un RawBit
+	*/
+
+	this->_RawBits = static_cast<int>(roundf(n*(1 << this->_IndComma)));
+	std::cout << "Float constructor called" << std::endl;
+
+}
+
+float Fixed::toFloat(void) const
+{
+	/*
+		Algo :
+			1) On convertit RawBit en un float
+			2) On divise ce float par 256 pour retrouver la valeur initiale
+		C++ se charge de donner à la mantisse et à l'exposant les bonnes valeurs.
+	*/
+
+	return static_cast<float>(this->_RawBits) / (1 << this->_IndComma);
+}
+
+int Fixed::toInt(void) const
+{
+	return (this->_RawBits >> _IndComma);
+}
+
+Fixed::Fixed(Fixed const &src)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
@@ -28,7 +61,6 @@ Fixed::~Fixed()
 
 int	Fixed::getRawBits() const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	(void) this->_IndComma;
 	return this->_RawBits;
 }
@@ -36,4 +68,17 @@ int	Fixed::getRawBits() const
 void Fixed::setRawBits(int const raw)
 {
 	this->_RawBits = raw;
+}
+
+Fixed&	Fixed::operator=(Fixed const &rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_RawBits = rhs.getRawBits();
+	return *this;
+}
+
+std::ostream& operator<<(std::ostream& o, Fixed const & i)
+{
+	o << i.toFloat();
+	return o;
 }
